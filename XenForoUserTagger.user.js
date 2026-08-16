6eacd0fa300c3c67fbe53fd569bd64be153167ac
yolotheswagnificent <benhankock@hotmail.com>
2026-08-16 19:22:04 +1000

    .

--- XenForoUserTagger.user.js
+++ XenForoUserTagger.user.js
@@ -2,7 +2,7 @@
 // @name         XenForo User Tagger (Inline UI)
 // @namespace    xf-user-tagger-inline-ui
 // @author       yolotheswagnificent
-// @version      1.1.0
+// @version      1.1.1
 // @description  Tag users with label + color using an inline UI under profiles
 // @match        *://*/*
 // @grant        none
